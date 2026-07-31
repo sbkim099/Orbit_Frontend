da0ccfa4c39dc597d7045f5675cada5338f1f9b7
정선경 <wjdtjsrud000@naver.com>
2026-07-31 19:38:30 +0900

    로그아웃 시 user + token 검사 -> 웹소켓 구독 종료되게 수정

--- src/components/websocket/NotificationProvider.jsx
+++ src/components/websocket/NotificationProvider.jsx
@@ -1,21 +1,27 @@
 import { useEffect } from "react";
 import { connectSocket, disconnectSocket } from "./websocket";
 import useUserStore from "../../store/userStore";
+import useAuthStore from "../../store/authStore";
 
 
 export default function NotificationProvider({ children }) {
 
     const user = useUserStore(state => state.user);
+    const token = useAuthStore(state => state.token);
 
     useEffect(() => {
-        if (!user?.id) return;
+        if (!user?.id || !token) {
+            disconnectSocket();
+            return;
+        }
+
         connectSocket(user.id);
 
         return () => {
             disconnectSocket();
         };
 
-    }, [user?.id]);
+    }, [user?.id, token]);
 
     return children;
 }
